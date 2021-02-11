f56620097b478defd5b082015972a2ce90e2a01b
Shaukat Ali <shaukat41@gmail.com>
2021-02-10 22:53:42 -0600

    Submitted JS Programming-Assignment 3-158834-CNC

---
+++ JS- Programing-assignment-3-158834-CNC/task2/task2.js
@@ -0,0 +1,4 @@
+var message1 = "karachi";
+var message2 = message1.toUpperCase();
+document.write(`karachi, <br> KARACHI`);
+alert("Match");
---
+++ JS- Programing-assignment-3-158834-CNC/task4/task4.js
@@ -0,0 +1,6 @@
+var text = `How bright they've grown in the sunlight!`;
+var ef = text.indexOf("!");
+if (ef =>0){
+    text = "Exclamation Found";
+}
+alert(text);
